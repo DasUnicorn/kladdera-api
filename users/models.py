423e8ba9f2be5b182897ec9837e6abc4b46260d2
dasUnicorn <info@dasunicorn.de>
2024-03-30 17:10:03 +0100

    fix: remove username from model

--- users/models.py
+++ users/models.py
@@ -2,26 +2,26 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
+    def create_user(self, email, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username)
+        user = self.model(email=email)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password):
-        user = self.create_user(email=email, username=username, password=password)
+    def create_superuser(self, email, password):
+        user = self.create_user(email=email, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+	username = None
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=255)
     current_streak = models.PositiveIntegerField(default=0)
 
     is_staff = models.BooleanField(default=False)
@@ -29,7 +29,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
