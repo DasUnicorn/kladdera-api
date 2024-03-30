09f7be87eb1a97d4e3665e0ea38243cfbc517e94
dasUnicorn <info@dasunicorn.de>
2024-03-30 16:34:21 +0100

    fix: change name to username to work with allauth

--- users/models.py
+++ users/models.py
@@ -2,18 +2,18 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, username, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, username=username)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name, password):
-        user = self.create_user(email=email, name=name, password=password)
+    def create_superuser(self, email, username, password):
+        user = self.create_user(email=email, username=username, password=password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -21,7 +21,7 @@ class CustomUserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
     current_streak = models.PositiveIntegerField(default=0)
 
     is_staff = models.BooleanField(default=False)
