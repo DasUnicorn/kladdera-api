b76835255562b1ac549ab1e02b5c4641a2559fd0
dasUnicorn <info@dasunicorn.de>
2024-04-03 02:26:05 +0200

    remove unused code

--- users/serializers.py
+++ users/serializers.py
@@ -16,15 +16,4 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', "password", 'current_streak']
-
-
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = CustomUser
-        fields = ['email', 'password', 'current_streak']
-
-    def create(self, validated_data):
-        return CustomUser.objects.create_user(**validated_data)
+        fields = ['email', 'password']
--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,7 @@
 from django.urls import path
-from .views import CustomUserViewSet, CreateUserView
+from .views import CreateUserView
 
 urlpatterns = [
-    path('user/', CustomUserViewSet.as_view({
-        'get': 'list',     # Endpoint to list all users
-        'post': 'create'   # Endpoint to create a new user
-    }), name='user-list'),
-
-    path('user/<int:pk>/', CustomUserViewSet.as_view({
-        'get': 'retrieve',    # Endpoint to retrieve a specific user
-        'put': 'update',      # Endpoint to update a specific user
-        'patch': 'partial_update',  # Endpoint to partially update a specific user
-        'delete': 'destroy'   # Endpoint to delete a specific user
-    }), name='user-detail'),
-
     # New registration endpoint
     path('register/', CreateUserView.as_view(), name='user-register'),
 ]
